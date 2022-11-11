c2a9f9ea12120bb00136b559b00240c3c7f141de
Angela0798 <117976387+Angela0798@users.noreply.github.com>
2022-11-11 10:50:28 +0100

    Update 11-11Angela.java

--- 11-11Angela.java
+++ 11-11Angela.java
@@ -5,13 +5,13 @@ class Esercizio1{
         double x='b';
         double y=123.34;
         double z=1;
-
+        //faccio la somma delle variabili e divido per 3
         double somma=x+y+z;
         double dividi3= (somma)/3;
-
+        //Valorizzazioni
         boolean Bool1=true;
         boolean Bool2=false;
-
+        //Stampo se entrambe le variabili bool sono uguali
         System.out.println(Bool1==!(Bool2));
         System.out.println(dividi3);
         System.out.println("Evviva");
