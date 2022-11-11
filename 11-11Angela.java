e9fdc16e942a5401e213febd735a58bcc03902b0
Angela0798 <117976387+Angela0798@users.noreply.github.com>
2022-11-11 10:43:15 +0100

    Esercizio1 Ungolo Angela 11-11-2022

---
+++ 11-11Angela.java
@@ -0,0 +1,19 @@
+class Esercizio1{
+    public static void main(String[] args)
+    {
+        //VALORIZZAZIONI
+        double x='b';
+        double y=123.34;
+        double z=1;
+
+        double somma=x+y+z;
+        double dividi3= (somma)/3;
+
+        boolean Bool1=true;
+        boolean Bool2=false;
+
+        System.out.println(Bool1==!(Bool2));
+        System.out.println(dividi3);
+        System.out.println("Evviva");
+    }
+}
